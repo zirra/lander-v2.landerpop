1ab7c6cd706cc0583e1cf8619a7bcec05fadc30c
zirra <msullivan@heysulli.com>
2025-10-16 14:26:50 -0500

    menu card

---
+++ src/components/cards/MenuCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="menu-card">
+    <img 
+      :src="`${imageRoot}${application.propertyId}/${context.image}`" 
+      @click="$router.push(context.routeName)"/><br/>
+    <p>
+      <a @click="$router.push(context.routeName)">View {{ context.segmentName }}</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MenuCard',
+  props: {
+    context: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="home">
     {{ appDomain }}<br/><br/>
-    <div v-for="(segment, id) in application.content"
+    <menu-card
+      v-for="(segment, id) in application.content"
       :key="id"
-      style="width: 94%; margin: 3%;">
-      <img 
-        :src="`${imageRoot}${application.propertyId}/${segment.image}`" 
-        @click="$router.push(segment.routeName)"/><br/>
-      <a 
-        @click="$router.push(segment.routeName)">View {{ segment.segmentName }}
-      </a>
-        <br/><br/>
-    </div>
+      :context="segment" />
   </div>
 </template>
 
 <script>
+import MenuCard from '@/components/cards/MenuCard.vue'
 export default {
+  components: { MenuCard },
   name: 'HomeView',
   data: () => ({
     appDomain: null
